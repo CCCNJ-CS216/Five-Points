e7971fa0e8692a88c1a9f7614c67c1a879e2d9a4
Nate Pisarski <nathanpisarski@gmail.com>
2015-09-10 17:07:21 -0400

    Add zip

--- src/edu/cccnj/FivePoints/General/Pair.java
+++ src/edu/cccnj/FivePoints/General/Pair.java
@@ -1,5 +1,7 @@
 package edu.cccnj.FivePoints.General;
 
+import java.util.ArrayList;
+
 /**
  * Pair is a general purpose class for coupling together
  * two objects of different or same type. (Java doesn't have tupples??)
@@ -28,7 +30,31 @@ public class Pair<T1, T2> {
         return this.item1 == otherPair.item1 && this.item2 == otherPair.item2;
     }
 
+    /**
+     * Convert the Pair to a String representation.
+     * @return The Pair as a string.
+     */
     public String toString(){
         return "item1: " + item1.toString() + " | item2: " + item2.toString();
     }
-}
+
+    /**
+     * Zip together two arrays to create a list of pairs. A mismatch in size will trucate.
+     * @param array1 - The first array (x)
+     * @param array2 - The second array (y)
+     * @param <T1> - The type of the first array
+     * @param <T2> - The type of the second array
+     * @return The arrays zipped together
+     */
+    public static <T1, T2> ArrayList<Pair<T1, T2>> zip(T1[] array1, T2[] array2){
+        ArrayList<Pair<T1, T2>> pairs = new ArrayList<>();
+
+        // Iterate over the first list
+        for(int i = 0; i < array1.length && i < array2.length; i++) {
+            // Bind it to the second array
+            pairs.add(new Pair<T1, T2>(array1[i], array2[i]));
+        }
+
+        return pairs;
+        }
+    }
