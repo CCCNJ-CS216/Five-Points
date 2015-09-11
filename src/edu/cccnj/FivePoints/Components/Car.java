4b10fee060212416d43b3db01f27a6157b8df80f
Nate Pisarski <nathanpisarski@gmail.com>
2015-09-10 23:12:14 -0400

    Add Car, Actor

---
+++ src/edu/cccnj/FivePoints/Components/Car.java
@@ -0,0 +1,22 @@
+package edu.cccnj.FivePoints.Components;
+import edu.cccnj.FivePoints.General.Actor;
+/**
+ * Write a description of class Car here.
+ * 
+ * @author (Joseph Marrongelli)
+ * @version (9/10/2015)
+ */
+public class Car implements Actor
+{
+    // instance variables - replace the example below with your own
+
+    /**
+     * Constructor for objects of class Car
+     */
+    public void act(int ticks)
+    {
+        //
+    }
+
+
+}
---
+++ src/edu/cccnj/FivePoints/General/Actor.java
@@ -0,0 +1,15 @@
+package edu.cccnj.FivePoints.General;
+
+/**
+ * An actor in the Five Points simulation.
+ * 
+ * @author "Totally" Joseph Marrongelli
+ * @version 9/10/2015
+ */
+public interface Actor
+{
+    /**
+     * Implement the actor's behavior.
+     */
+    public void act(int tick);
+}
