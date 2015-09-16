2d27e1562a0ed74c3956cf7f3670093d20a4353d
Nate Pisarski <nathanpisarski@gmail.com>
2015-09-16 13:54:47 -0400

    Add Car with stubs to compile

--- src/edu/cccnj/FivePoints/Components/Car.java
+++ src/edu/cccnj/FivePoints/Components/Car.java
@@ -1,22 +1,66 @@
 package edu.cccnj.FivePoints.Components;
+
 import edu.cccnj.FivePoints.General.Actor;
+
 /**
  * Write a description of class Car here.
- * 
- * @author (Joseph Marrongelli)
- * @version (9/10/2015)
+ *
+ * @author (Joseph Marrongelli / Warren Devonshire)
+ * @version (9/16/2015)
  */
 public class Car implements Actor
 {
-    // instance variables - replace the example below with your own
+    class Route{Lane getFirstLane(){return new Lane();}Lane getNextLane(){return this.getFirstLane();}void recordTime(Lane l,int ticks){}}
+    class Lane{boolean inFront(Car c){return false;}void changeLane(Car c, Lane l){}}
+    /**
+     * List of Lanes and entry time
+     */
+    private Route myRoute;
+
+    /**
+     * Lane the car is currently in
+     */
+    private Lane currentLane;
+
+    /**
+     * Lane car will be in after the current lane
+     */
+    private Lane nextLane;
 
     /**
      * Constructor for objects of class Car
      */
-    public void act(int ticks)
+    public Car(Route r)
     {
-        //
+        this.myRoute = r;
+
+        // Assuming CarSource puts the car in the first Lane
+        this.currentLane = myRoute.getFirstLane();
+
+        this.nextLane = myRoute.getNextLane();
     }
 
+    /**
+     * Check to see if this car can (and should) move lanes.
+     * @param ticks The current tick count.
+     */
+    public void act(int ticks)
+    {
+        //checks to see if 'this' car is in from of current lane.(at intersection)
+        if(currentLane.inFront(this)){
+
+            //ask lane to remove 'this' car from itself, and add it to the nextLane of the route.
+            currentLane.changeLane(this, nextLane);
+
+            //once lanes have changed, currentLane becomes nextLane
+            currentLane = nextLane;
+
+            //record tick for the lane just entered.
+            myRoute.recordTime(currentLane, ticks);
+
+            //find out what the next step in the route will be from Route, store in 'nextLane'
+            nextLane = myRoute.getNextLane();
+        }
+    }
 
 }
