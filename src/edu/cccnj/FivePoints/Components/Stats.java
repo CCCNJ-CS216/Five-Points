6b4dd629cf01cff9083d49baf8e42b1d38f1edf0
Nate Pisarski <nathanpisarski@gmail.com>
2015-09-29 17:12:14 -0400

    Add Stats class ~ Nancy Matias

---
+++ src/edu/cccnj/FivePoints/Components/Stats.java
@@ -0,0 +1,35 @@
+package edu.cccnj.FivePoints.Components;
+
+import edu.cccnj.FivePoints.General.Actor;
+
+import java.util.ArrayList;
+/**
+ * Write a description of class Stats here.
+ *
+ * @author (Nancy)
+ * @version (9.15.15)
+ */
+public class Stats implements Actor
+{
+    private ArrayList<Actor> actors;
+
+    /**
+     * Constructor for objects of class Stats
+     */
+    public Stats(ArrayList<Actor> a)
+    {
+        actors = a;
+    }
+
+    public void act(int time)
+    {
+
+    }
+
+    public int currentCount()
+    {
+        // Ask the lane for the max count?
+        return -1; // TODO: not this
+    }
+
+}
